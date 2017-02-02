aa49a4431b997487093bf17916aab899c08dea2b
Hyungmo Gu <guhyungm7@gmail.com>
2017-02-02 01:35:38 -0800

    Delete unimportant files

--- database_setup.py
+++
@@ -1,9 +0,0 @@
-from google.appengine.ext import db
-
-#GQL automatically adds key ID when an entry is added
-
-class Blog(db.Model):
-	title = db.StringProperty(required=True)
-	content = db.TextProperty(required=True)
-	dateCreated = db.DateTimeProperty(auto_now_add=True)
-
